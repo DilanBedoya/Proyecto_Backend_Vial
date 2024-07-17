497d48d34a7c721947f281119bd93aa2cc46a7e1
DilanBedoya <estilius2@hotmail.com>
2024-07-17 01:29:33 -0500

    Documentos Swagger

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import app from './server.js'
 import connection from './database.js';
+import { V1SwaggerDocs } from "../docs/swagger.js";
 
 connection()
 
 app.listen(app.get('port'),() => {
+    V1SwaggerDocs(app, app.get("port"));
     console.log(`Server ok on http://localhost:${app.get('port')}`);
 })
